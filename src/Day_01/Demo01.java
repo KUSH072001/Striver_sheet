982f4101fe3cee17947c6232386280b7d1195fe2
Krishna Deshmukh <krishnadesh2001@gmail.com>
2025-08-28 17:09:11 +0530

    first question pattern

--- src/Day_01/Demo01.java
+++ src/Day_01/Demo01.java
@@ -1,5 +1,12 @@
 package Day_01;
 
-public class Demo01
-{
+public class Demo01 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
